e1ff27d550b0b8817889afb2310ac2daf5656b30
rcferreira232 <rafaelcferreira232@gmail.com>
2026-08-09 11:54:43 -0300

    feat: alias config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
 import { app } from "./app.ts";
-import { env } from "./config/env.ts";
+import { env } from "./config/env";
 
 const start = async () => {
   try {
     app.listen(env.port, () => {
-      console.log(`Server is running on ${env.port}`);
+      console.log(`Server is running on port: ${env.port}`);
     });
   } catch (error) {
     console.error(error);
