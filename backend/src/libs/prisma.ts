a6705f9f164dad7ef3c69a0ce1fb965f683e765f
rcferreira232 <rafaelcferreira232@gmail.com>
2026-08-09 11:54:07 -0300

    fix: att Prisma output path e spec.md lib->libs

--- backend/src/libs/prisma.ts
+++ backend/src/libs/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client.ts";
-import { env } from "../config/env.ts";
+import { PrismaClient } from "@/generated/prisma/client";
+import { env } from "@/config/env";
 
 const connectionString = `${env.postgresUrl}`;
 const adapter = new PrismaPg({ connectionString });
