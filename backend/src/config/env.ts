07c5887f6a43bfedfb2c87609a59c54c585eff81
rcferreira232 <rafaelcferreira232@gmail.com>
2026-08-10 10:03:40 -0300

    att env.ts configs

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,9 +1,13 @@
 type Env = {
   postgresUrl: string;
   port: number;
+  jwtSecret: string;
+  tmdbApiKey: string;
 };
 
 export const env: Env = {
   port: Number(process.env.APP_PORT) || NaN,
   postgresUrl: process.env.DATABASE_URL || "",
+  jwtSecret: process.env.JWT_SECRET || "",
+  tmdbApiKey: process.env.TMDB_API_KEY || "",
 };
