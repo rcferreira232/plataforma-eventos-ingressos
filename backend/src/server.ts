30fc6b32babcc347532255db997345cd54b5fd2c
rcferreira232 <rafaelcferreira232@gmail.com>
2026-08-13 12:07:56 -0300

    teste ddeploy server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import { env } from "./config/env";
 
 const start = async () => {
   try {
-    app.listen(3000, () => {
+    app.listen(env.port, () => {
       console.log(`Server is running on port: ${env.port}`);
     });
   } catch (error) {
